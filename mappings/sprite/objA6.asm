e4a6348be64e608f11ce1581263d6568b86a9a36
KCEXE <robbiem1922@outlook.com>
2026-02-16 02:11:45 -0500

    Fixed Springs in SonLVL
    It was that easy™

--- mappings/sprite/objA6.asm
+++ mappings/sprite/objA6.asm
@@ -1,61 +1,44 @@
-Map_objA6:	mappingsTable
-	mappingsTableEntry.w	Map_objA6_0010
-	mappingsTableEntry.w	Map_objA6_0032
-	mappingsTableEntry.w	Map_objA6_0054
-	mappingsTableEntry.w	Map_objA6_0076
-	mappingsTableEntry.w	Map_objA6_0098
-	mappingsTableEntry.w	Map_objA6_00BA
-	mappingsTableEntry.w	Map_objA6_00DC
-	mappingsTableEntry.w	Map_objA6_00E6
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 2 format
+; --------------------------------------------------------------------------------
 
-Map_objA6_0010:	spriteHeader
-	spritePiece	-8, -$C, 1, 1, 0, 0, 0, 0, 0
-	spritePiece	-$18, -4, 3, 2, 1, 0, 0, 0, 0
-	spritePiece	0, -$C, 1, 1, 0, 1, 0, 0, 0
-	spritePiece	0, -4, 3, 2, 1, 1, 0, 0, 0
-Map_objA6_0010_End
-
-Map_objA6_0032:	spriteHeader
-	spritePiece	-8, -$C, 1, 1, 0, 0, 0, 0, 0
-	spritePiece	-$18, -4, 3, 2, 7, 0, 0, 0, 0
-	spritePiece	0, -$C, 1, 1, 0, 1, 0, 0, 0
-	spritePiece	0, -4, 3, 2, 7, 1, 0, 0, 0
-Map_objA6_0032_End
-
-Map_objA6_0054:	spriteHeader
-	spritePiece	-$10, -$C, 2, 1, $D, 0, 0, 0, 0
-	spritePiece	-$18, -4, 3, 2, 1, 0, 0, 0, 0
-	spritePiece	0, -$C, 2, 1, $D, 1, 0, 0, 0
-	spritePiece	0, -4, 3, 2, 1, 1, 0, 0, 0
-Map_objA6_0054_End
-
-Map_objA6_0076:	spriteHeader
-	spritePiece	-$C, -$18, 2, 3, $F, 0, 0, 0, 0
-	spritePiece	4, -8, 1, 1, $15, 0, 0, 0, 0
-	spritePiece	-$C, 0, 2, 3, $F, 0, 1, 0, 0
-	spritePiece	4, 0, 1, 1, $15, 0, 1, 0, 0
-Map_objA6_0076_End
-
-Map_objA6_0098:	spriteHeader
-	spritePiece	-$C, -$18, 2, 3, $16, 0, 0, 0, 0
-	spritePiece	4, -8, 1, 1, $15, 0, 0, 0, 0
-	spritePiece	-$C, 0, 2, 3, $16, 0, 1, 0, 0
-	spritePiece	4, 0, 1, 1, $15, 0, 1, 0, 0
-Map_objA6_0098_End
-
-Map_objA6_00BA:	spriteHeader
-	spritePiece	-$C, -$18, 2, 3, $F, 0, 0, 0, 0
-	spritePiece	4, -$10, 1, 2, $1C, 0, 0, 0, 0
-	spritePiece	-$C, 0, 2, 3, $F, 0, 1, 0, 0
-	spritePiece	4, 0, 1, 2, $1C, 0, 1, 0, 0
-Map_objA6_00BA_End
-
-Map_objA6_00DC:	spriteHeader
-	spritePiece	-4, -4, 1, 1, $1E, 0, 0, 0, 0
-Map_objA6_00DC_End
-
-Map_objA6_00E6:	spriteHeader
-	spritePiece	-4, -4, 1, 1, $1F, 0, 0, 0, 0
-Map_objA6_00E6_End
-
-	even
+SME_bJueq:	
+		dc.w SME_bJueq_10-SME_bJueq, SME_bJueq_32-SME_bJueq	
+		dc.w SME_bJueq_54-SME_bJueq, SME_bJueq_76-SME_bJueq	
+		dc.w SME_bJueq_98-SME_bJueq, SME_bJueq_BA-SME_bJueq	
+		dc.w SME_bJueq_DC-SME_bJueq, SME_bJueq_E6-SME_bJueq	
+SME_bJueq_10:	dc.b 0, 4	
+		dc.b $F4, 0, 0, 0, 0, 0, $FF, $F8	
+		dc.b $FC, 9, 0, 1, 0, 0, $FF, $E8	
+		dc.b $F4, 0, 8, 0, 8, 0, 0, 0	
+		dc.b $FC, 9, 8, 1, 8, 0, 0, 0	
+SME_bJueq_32:	dc.b 0, 4	
+		dc.b $F4, 0, 0, 0, 0, 0, $FF, $F8	
+		dc.b $FC, 9, 0, 7, 0, 3, $FF, $E8	
+		dc.b $F4, 0, 8, 0, 8, 0, 0, 0	
+		dc.b $FC, 9, 8, 7, 8, 3, 0, 0	
+SME_bJueq_54:	dc.b 0, 4	
+		dc.b $F4, 4, 0, $D, 0, 6, $FF, $F0	
+		dc.b $FC, 9, 0, 1, 0, 0, $FF, $E8	
+		dc.b $F4, 4, 8, $D, 8, 6, 0, 0	
+		dc.b $FC, 9, 8, 1, 8, 0, 0, 0	
+SME_bJueq_76:	dc.b 0, 4	
+		dc.b $E8, 6, 0, $F, 0, 7, $FF, $F4	
+		dc.b $F8, 0, 0, $15, 0, $A, 0, 4	
+		dc.b 0, 6, $10, $F, $10, 7, $FF, $F4	
+		dc.b 0, 0, $10, $15, $10, $A, 0, 4	
+SME_bJueq_98:	dc.b 0, 4	
+		dc.b $E8, 6, 0, $16, 0, $B, $FF, $F4	
+		dc.b $F8, 0, 0, $15, 0, $A, 0, 4	
+		dc.b 0, 6, $10, $16, $10, $B, $FF, $F4	
+		dc.b 0, 0, $10, $15, $10, $A, 0, 4	
+SME_bJueq_BA:	dc.b 0, 4	
+		dc.b $E8, 6, 0, $F, 0, 7, $FF, $F4	
+		dc.b $F0, 1, 0, $1C, 0, $E, 0, 4	
+		dc.b 0, 6, $10, $F, $10, 7, $FF, $F4	
+		dc.b 0, 1, $10, $1C, $10, $E, 0, 4	
+SME_bJueq_DC:	dc.b 0, 1	
+		dc.b $FC, 0, $60, $1E, $60, $F, $FF, $FC	
+SME_bJueq_E6:	dc.b 0, 1	
+		dc.b $FC, 0, $60, $1F, $60, $F, $FF, $FC	
+		even
